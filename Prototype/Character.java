a366729999514403c0e85632ab86483cbf7184e7
lebrancconvas <lebrancconvas@gmail.com>
2021-09-22 04:58:00 +0700

    :sparkles: Add Inheritance Code.

--- Prototype/Character.java
+++ Prototype/Character.java
@@ -1,8 +1,49 @@
-package Prototype;
-
-public class Character {
-	public int level;
-	public String name;
-	public int HP;
-	public int MP; 
-}
+package Prototype; 
+class Character {
+	public static void main(String[] args) {
+	  Warrior ramsa = new Warrior(1,"ramsa");
+	  ramsa.sword = "Buster Sword"; 
+	  ramsa.callClass();
+	  ramsa.attack();
+	  
+	  Mage montblanc = new Mage(2,"Monblanc");
+	  montblanc.wand = "Olywander Wand";
+	  montblanc.callClass();
+	  montblanc.attack();  
+	}
+  }
+  
+  class JobClass {
+	int id;
+	String name;
+	public JobClass(){}
+	public JobClass(int id, String name) {
+	  this.id = id;
+	  this.name = name;
+	}
+	public void callClass() {
+	  System.out.println("Class Appear.");
+	}
+  }
+  
+  class Warrior extends JobClass {
+	String sword;
+	public Warrior(int id, String name) {
+	  this.id = id;
+	  this.name = name;
+	}
+	public void attack() {
+	  System.out.printf("Warrior attacked with %s\n", sword);
+	}
+  }
+  
+  class Mage extends JobClass {
+	String wand;
+	public Mage(int id, String name) {
+	  this.id = id;
+	  this.name = name; 
+	}
+	public void attack() {
+	 System.out.printf("Mage attacked with %s\n", wand);  
+	}
+  }
