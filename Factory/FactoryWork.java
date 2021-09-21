3559a4944763c65e894610fd8907c2479aeb0331
lebrancconvas <lebrancconvas@gmail.com>
2021-09-21 15:21:33 +0700

    :sparkles: Add Character.

--- Factory/FactoryWork.java
+++ Factory/FactoryWork.java
@@ -3,7 +3,11 @@ package Factory;
 public class FactoryWork {
 	public static void main(String[] args) {
 		CharacterFactory gamechar = new CharacterFactory(); 
+
 		GameCharacter Naduin = gamechar.getCharacter("WARRIOR");
 		Naduin.attack(); 
+		
+		GameCharacter Manchury = gamechar.getCharacter("MAGE");
+		Manchury.attack(); 
 	}
 }
