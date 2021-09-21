7855286c55f6b6752a0f12c6c4b6d746196d032f
lebrancconvas <lebrancconvas@gmail.com>
2021-09-21 20:10:23 +0700

    :recycle: Refactor Boolean Variable.

--- Singleton/SingletonWork.java
+++ Singleton/SingletonWork.java
@@ -4,13 +4,9 @@ public class SingletonWork {
 	public static void main(String[] args) {
 		BossCharacter Namu = BossCharacter.getBoss();
 		BossCharacter Mame = BossCharacter.getBoss();
-		if(Namu == Mame) {
-			System.out.println(true);
-		}
-		else {
-			System.out.println(false);
-		}
+		Boolean checkBoss = ((Namu == Mame) ? true : false);
+		System.out.println(checkBoss); 
 		Namu.bossAttack();
-		Mame.bossAttack(); 
+		Mame.bossAttack();  
 	}	
 }
